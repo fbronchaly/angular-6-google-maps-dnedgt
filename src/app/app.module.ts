8b57e1b46a65b80c8d133660396c9e0e47f6a171
fbronchaly <fbronchaly@gmail.com>
2020-07-26 07:30:21 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAFgM81Qz-SwfTzUsr4F51AgDj0HdN88CQ'
+    })
+  ],
+  providers: [],
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {}
